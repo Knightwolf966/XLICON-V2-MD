e605fef8d80eda55c60d939b472b841bd34d4cb9
Knightwolf966 <156714737+Knightwolf966@users.noreply.github.com>
2024-01-17 13:42:47 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['918360234087', 'WHITE', true], 
+  ['256726284991', 'Ｏp・ȚHÜĞ', true], 
   ['233208179431', 'Abraham Dwamena', true],
   ['923184070915', '𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿 -(𝘼𝙝𝙢𝙢𝙞)', true], 
   ['94789481495', '𝙼𝚉𝙽 𝙺𝙸𝙽𝙶 ⚡', true],
@@ -19,8 +19,8 @@ global.owner = [
 //global.pairingNumber = "" //put your bot number here
 
 global.mods = ['923184070915', '94789481495', '923184474176','33757054415','233208179431'] 
-global.prems = ['923184070915', '233208179431', '94789481495', '918360234087','33757054415']
-global.allowed = ['923184070915', '233208179431', '94789481495', '918360234087','33757054415']
+global.prems = ['923184070915', '256726284991', '94789481495', '918360234087','33757054415']
+global.allowed = ['923184070915', '256726284991', '94789481495', '918360234087','33757054415']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -57,7 +57,7 @@ global.APIKeys = { // APIKey Here
 // Sticker WM
 global.botname = 'XLICON-V2'
 global.premium = 'true'
-global.packname = 'XLICON TEAM' 
+global.packname = 'Ｏp・ȚHÜĞ BOT' 
 global.author = '@XLICON-V2' 
 global.menuvid = 'https://telegra.ph/file/325630f66abc968eda8e2.mp4'
 global.igfg = '▢✓ Follow My channel\nhttps://whatsapp.com/channel/0029VaE8GbCDzgTILE7OtC3e\n' 
